e0716ba5743102a7f7d01c5836d08b060fe516e3
Majd Murad <murad.majd@gmail.com>
2017-01-14 14:30:36 -0500

    Filter by type working

--- reactjs/src/venue_list.js
+++ reactjs/src/venue_list.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import Api from './services/api';
 import VenueListItem from './venue_list_item'
+import _ from 'lodash'
 
 class VenueList extends Component {
 	constructor(props) {
     	super(props);
     	this.state = { 
-    		venues: []
+    		venues: [],
+    		filters: [],
+    		filteredVenues: []
     	};
   	}
 
@@ -18,20 +21,54 @@ class VenueList extends Component {
 		const api = new Api();
 		api.getVenues()
 			.then(venues => {
+				const filters = venues.map(venue => {
+					return (venue.type.charAt(0).toUpperCase() + venue.type.slice(1))
+				})
 				this.setState({
-					venues: venues
+					venues: venues,
+					filters: filters,
+					filteredVenues: venues
 				})
 			});
 	}
 
+	showAll() {
+		this.setState({
+			filteredVenues: this.state.venues
+		})
+	}
+
+	filterByType(type) {
+		const filteredVenues = _.filter(this.state.venues, 
+			(venue) => venue.type === type.toLowerCase()
+		)
+		this.setState({
+			filteredVenues: filteredVenues
+		})
+	}
+
 	render() {
+		const { filteredVenues, filters } = this.state
 		return (
-			<div className="list">
-				{ this.state.venues.map((venue, index) => {
-					return (
-						<VenueListItem key={index} venue={venue} />
+			<div className="venues">
+				<div className="filters">
+					Quick Filters:
+					<div onClick={this.showAll.bind(this)}>All</div>
+					{ filters.map((filter, index) => {
+						return (
+							<div key={index} onClick={this.filterByType.bind(this, filter)}>
+								{filter}
+							</div>
+						)}
 					)}
-				)}
+				</div>
+				<div className="venue-list">
+					{ filteredVenues.map((venue, index) => {
+						return (
+							<VenueListItem key={index} venue={venue} />
+						)}
+					)}
+				</div>
 			</div>
 		);
 	}
